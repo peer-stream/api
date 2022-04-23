c5186790967ce5854ec5c92d96159854b5a1ef0f
Ahmet Aygün <me@ahmetaygun.net>
2022-04-23 03:04:33 +0200

    check JWT_SECRET env var before spinning up server and added generic error handler

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+const assert = require('assert');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const dotenv = require('dotenv').config();
+
+assert(process.env.JWT_SECRET, 'JWT_SECRET environment variable must be provided')
 
 const indexRouter = require('./routes/index');
 const authRouter = require('./routes/auth');
@@ -16,5 +19,10 @@ app.use(cookieParser());
 
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500)
+    res.json(err)
+})
 
 module.exports = app;
