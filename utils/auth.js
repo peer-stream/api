4dd5aa902e77a557ab5f8c79509af60bd27018df
Ahmet Aygün <me@ahmetaygun.net>
2022-04-23 03:19:22 +0200

    moved auth nonce message building under utils

---
+++ utils/auth.js
@@ -0,0 +1,7 @@
+const buildMessage = (address, nonce) => `!! PLEASE DON'T change this message as this will fail your login request. !!\n
+  You requested to login to PeerStream with your wallet and when you sign this message we'll sign you in.\n
+  It will NOT cost you anything.\n
+  Wallet Address: ${address}\n\n
+  Unique request ID which your wallet and our app will handshake upon is: [${nonce}]`
+
+module.exports = buildMessage;
