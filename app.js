dce69318a98b4c5acd042e1774835f9cfcdb0327
Ahmet Aygün <me@ahmetaygun.net>
2022-04-23 15:41:09 +0200

    added cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const assert = require('assert');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 const dotenv = require('dotenv').config();
 
 assert(process.env.JWT_SECRET, 'JWT_SECRET environment variable must be provided')
@@ -19,7 +20,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
+app.options('*', cors());
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
 app.use('/streams', streamRouter);
