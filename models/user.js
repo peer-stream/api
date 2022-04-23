ae43a16febff7c1f507555a6ea3bf687143bee9d
Ahmet Aygün <me@ahmetaygun.net>
2022-04-23 03:18:52 +0200

    added user model

---
+++ models/user.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const { nanoid } = require('nanoid');
+
+const userSchema = new mongoose.Schema(
+  {
+    _id: {
+      type: String,
+      default: () => nanoid()
+    },
+    display_name: {
+      type: String,
+      default: ''
+    },
+    wallet_address: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+      lowercase: true,
+      validate: (address) => {
+        return /^0x([A-Fa-f0-9]{64})$/.test(address);
+      }
+    },
+    auth_nonce: {
+      type: String,
+      default: () => nanoid(),
+      required: true,
+      unique: true,
+      index: true,
+      select: false
+    }
+  },
+  {
+    collection: 'users',
+    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
+    toJSON: {
+      transform: (doc, ret) => {
+        delete ret._id
+        delete ret.__v
+        delete ret.auth_nonce
+
+        ret.id = doc._id
+      }
+    }
+  },
+);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
